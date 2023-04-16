11e19948c14b8923b2ffdf2b8fa83f69de3d338f
Nathanael King <37662828+DeviousWings@users.noreply.github.com>
2023-04-16 15:04:33 -0500

    made comments

--- services/index.js
+++ services/index.js
@@ -52,6 +52,8 @@ export const getCategories = async () => {
 
   return result.categories;
   }
+
+
 export const getRecentPosts = async () => {
   const query = gql `
   query GetPostDetails() {
@@ -142,3 +144,18 @@ export const submitComment = async (obj) => {
 
   return result.json();
 };
+
+export const getComments = async (slug) => {
+  const query = gql`
+    query GetComments($slug: String!) {
+      comments(where: {post: { slug: $slug } } ) {
+        name
+        createdAt
+        comment
+      }
+    }
+  `;
+  const result = await request(graphqlAPI, query, { slug });
+
+  return result.comments;
+  }
