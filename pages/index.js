2c0a413d3d26991216b87dea77581f28ab6d2ce9
Nathanael King <37662828+DeviousWings@users.noreply.github.com>
2023-05-22 14:16:56 -0500

    connected mysql

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import { PostCard, Categories, PostWidget, AllPosts } from '../components';
 import { getPosts } from '../services'
+import { BrowserRouter as Routes, Switch, Route } from "react-router-dom";
 
 
 
@@ -13,6 +14,7 @@ export default function Home({ posts }) {
 
         <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
           <div className='lg:col-span-8 col-span-1'>
+
             {posts.map((post) => <PostCard post={post.node} key={post.title} />)}
           </div>
           <div className="lg:col-span-4 col-span-1">
